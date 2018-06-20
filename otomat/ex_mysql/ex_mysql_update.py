3821f76832facb1307c647af446ef85bce4bc08c
Andre Yang <yangjunfei2146@126.com>
2018-06-20 09:44:44 +0800

    remove aboute keysword

--- otomat/ex_mysql/ex_mysql_update.py
+++ otomat/ex_mysql/ex_mysql_update.py
@@ -1,8 +1,8 @@
 
 import MySQLdb
 
-con = MySQLdb.connect('localhost','rain','che001','online')
-with con:
+con = MySQLdb.connect('localhost','rain','xxx001','online')
+with con:p
 	curl = con.cursor()
 	curl.execute("update Writers SET Name=%s where Id=%s",("Yangjunfei","5"))
 	print "Number of rows updated:%d" % curl.rowcount
