1c4aa174b5859d9889a9168f9f02313117290a59
Andre Yang <yangjunfei2146@126.com>
2018-06-20 10:24:11 +0800

    remove about keysword

--- platform-manager/action_tenant_work_check.py
+++ platform-manager/action_tenant_work_check.py
@@ -61,7 +61,7 @@ def env_set_func(tenant_name):
 				'OS_TENANT_NAME':tenant_name_key,
 				#'OS_USERNAME':'kycloudprod',
 				'OS_USERNAME':user_name_key,
-				#'OS_PASSWORD':'che001'
+				#'OS_PASSWORD':'xxx001'
 				'OS_PASSWORD':password_key
 				}	
 
